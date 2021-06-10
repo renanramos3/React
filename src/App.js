3d0cbc30686257e210485cfd7183722e2b39e42d
Renan Ramos <renanramos3@gmail.com>
2021-06-10 10:14:09 -0300

    Add files via upload
    Meu primeiro projeto utilizando React que extrai dados de uma API, não me preocupei com o design, foco desse projeto é React!

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./styles.css";
+
+const App = () => {
+  const [contacts, setContacts] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://randomuser.me/api/?results=3")
+      .then(response => response.json())
+      .then(data => {
+        setContacts(data.results);
+      });
+  }, []);
+
+  return (
+    <>
+      {contacts.map(contact => (
+        <ContactCard
+          avatar={contact.picture.large}
+          name={contact.name.first + " " + contact.name.last}
+          email={contact.email}
+          age={contact.dob.age}
+        />
+      ))}
+    </>
+  );
+};
+
+const ContactCard = props => {
+  const [showAge, setShowAge] = React.useState(false);
+
+  return (
+    <div className="contact-card">
+      <img src={props.avatar} alt="profile" />
+      <div className="user-details">
+        <p>Name: {props.name}</p>
+        <p>Email: {props.email}</p>
+        <button onClick={() => setShowAge(!showAge)}>
+			Toggle Age 
+		</button>
+        {showAge && <p>Age: {props.age}</p>}
+      </div>
+    </div>
+  );
+};
+
+export default App;
